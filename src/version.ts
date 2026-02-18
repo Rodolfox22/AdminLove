2f79661eaa47e7be9df567fecc06912273c09b7d
Pablo Correa <pejotace22@gmail.com>
2026-02-18 11:40:38 -0300

    feat: actualizar a version 1.0.6

--- src/version.ts
+++ src/version.ts
@@ -1,5 +1,5 @@
 // Auto-generado por scripts/version.js
 // NO EDITAR MANUALMENTE
-export const VERSION = '1.0.5';
+export const VERSION = '1.0.6';
 export const APP_NAME = 'adminlove';
-export const BUILD_DATE = '2026-02-18T05:08:39.957Z';
+export const BUILD_DATE = '2026-02-18T14:38:00.508Z';
