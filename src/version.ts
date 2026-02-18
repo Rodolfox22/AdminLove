74579bb311894d648c30837dc84661efa8e4abc9
Pablo Correa <pejotace22@gmail.com>
2026-02-18 01:58:00 -0300

    feat: actualizar version a 1.0.4, reparar bug de deploy

--- src/version.ts
+++ src/version.ts
@@ -1,5 +1,5 @@
 // Auto-generado por scripts/version.js
 // NO EDITAR MANUALMENTE
-export const VERSION = '1.0.3';
+export const VERSION = '1.0.4';
 export const APP_NAME = 'adminlove';
-export const BUILD_DATE = '2026-02-10T04:16:57.735Z';
+export const BUILD_DATE = '2026-02-18T04:54:29.042Z';
