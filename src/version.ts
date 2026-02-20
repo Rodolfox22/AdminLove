6d29d002300c849dc1a130fb071cb298810e93c9
Pablo Correa <pejotace22@gmail.com>
2026-02-19 22:46:15 -0300

    feat: bump version to 1.0.7

--- src/version.ts
+++ src/version.ts
@@ -1,5 +1,5 @@
 // Auto-generado por scripts/version.js
 // NO EDITAR MANUALMENTE
-export const VERSION = '1.0.6';
+export const VERSION = '1.0.7';
 export const APP_NAME = 'adminlove';
-export const BUILD_DATE = '2026-02-18T14:38:00.508Z';
+export const BUILD_DATE = '2026-02-20T01:45:16.603Z';
